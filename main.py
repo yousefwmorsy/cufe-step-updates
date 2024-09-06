79310918a9b5bcc60ad3d962c24cdb9a1bd3162d
Yousef <yousefwmorsy@gmail.com>
2024-09-06 21:26:30 +0300

    improvements

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -12,38 +13,30 @@ import csv
 import codecs
 import shutil
 
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 app_path = os.path.dirname(sys.executable)
 now = datetime.now()
 day_month_year = now.strftime("%d%m%Y")
 
-#def filter_new_updates(previous, new, output_file):
-    # Load the data from the CSV files
-    #previous_updates = pd.read_csv(previous)
-    #new_updates = pd.read_csv(new)
-    #diff = previous_updates.compare(new_updates)
-
-
 def filter_new_updates(previous, output_file):
-    print("Program Run")
-    # Load the data from the CSV files
+    logging.info("filter_new_updates called")
+    logging.info("Program Run")
     previous_updates = pd.read_csv(previous)
     get_newest()
     new_updates = pd.read_csv(previous)
-
-    # Compare the two DataFrames
     diff = new_updates.merge(previous_updates, indicator=True, how='outer').loc[lambda x: x['_merge'] != 'both']
 
     if not diff.empty:
-        # Save the differences to a new CSV file
         diff.to_csv(output_file, index=False)
         send_msg(u"difference.csv")
 
-
 def get_newest():
+    logging.info("get_newest called")
     website = 'https://eng.cu.edu.eg/ar/credit-hour-system/'
-    path = "D:\Yousef\Downloads\chromedriver-win32\chromedriver-win32\chromedriver.exe"  # introduce path here
+    path = "D:\Yousef\Downloads\chromedriver-win32\chromedriver-win32\chromedriver.exe"
 
-    # add headless mode
     options = Options()
     options.headless = True
     options.add_argument("--headless=new")
@@ -99,19 +92,15 @@ def get_newest():
     my_dict = {'title': titles, 'subtitle': subtitles, 'link': links, 'link2': links2, 'link3': links3, 'image': images}
     df_headlines = pd.DataFrame(my_dict)
     file_name = 'previous_announcement.csv'
-
-    #final_path = os.path.join(app_path, file_name) #exe
-    #df_headlines.to_csv(final_path, index=False) #exe
-
     df_headlines.to_csv(file_name, index=False)
 
 def job():
+    logging.info("job called")
     filter_new_updates("previous_announcement.csv", "difference.csv")
 
 if __name__ == "__main__":
+    logging.info("Script started")
     schedule.every(20).seconds.do(job)
     while True:
         schedule.run_pending()
         time.sleep(1)
-
-
